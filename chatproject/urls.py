0de2f2ca75d2f5767ffee3504afde2576efa6b12
mishal <muhammedmishalc.kmechil@gmail.com>
2026-06-24 13:53:02 +0530

    Update urls.py

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -4,9 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # Chat App URLs
+
     path("", include("chat.urls")),
 
-    # Accounts App URLs
     path("accounts/", include("accounts.urls")),
 ]
